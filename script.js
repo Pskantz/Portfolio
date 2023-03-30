1688fe24f2f009b4b93e0ace9aeb9361ce607023
Pskantz <112848766+Pskantz@users.noreply.github.com>
2023-03-30 15:10:40 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Fullstack open source ", "JavaScript", " på Nackademin"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
